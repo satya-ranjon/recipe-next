4cd05e6b899b9428bea1c24208c3c3791da4fe1f
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-18 11:47:39 +0600

    feat: Implement DeleteRecipe component for recipe deletion
    This commit introduces the 'DeleteRecipe' component, which provides a button for users to delete a recipe. The component uses the 'Swal' (SweetAlert) library to confirm the deletion with a pop-up dialog. If the user confirms, a success message is displayed, and the user is redirected to the home page using Next.js 'useRouter' hook. The 'Delete' button includes icons for better user experience.

---
+++ src/components/recipe/DeleteRecipe.js
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Button from "../common/Button";
+import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
+
+const DeleteRecipe = ({ id }) => {
+  const router = useRouter();
+
+  const handleDelete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#fb923c",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your recipe has been deleted.",
+          icon: "success",
+          confirmButtonColor: "#fb923c",
+        });
+        router.push("/");
+      }
+    });
+  };
+  return (
+    <Button onClick={handleDelete}>
+      <span className=" md:hidden text-xl">
+        <MdDelete />
+      </span>
+      <span className=" hidden md:block text-lg font-normal">Delete</span>
+    </Button>
+  );
+};
+
+export default DeleteRecipe;
