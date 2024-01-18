c5eebfb579f7c4e8da3ab2be7a4606a5f67f7b47
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-18 11:15:23 +0600

    feat: Implement useOutsideClick hook for detecting clicks outside a specified ref
    This commit introduces the 'useOutsideClick' hook, a custom React hook that detects clicks outside a specified ref element. It takes a ref and a callback function as parameters. The hook adds a click event listener to the document and invokes the callback when a click occurs outside the provided ref. This can be useful for handling outside clicks, such as closing modals or dropdowns.

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+
+const useOutsideClick = (ref, callback) => {
+  const handleOutsideClick = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+};
+
+export default useOutsideClick;
